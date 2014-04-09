1de8b9c8072d9106878b65cf8b21cc57cbe797b0
GEnghusen <guie_rs@hotmail.com>
2014-04-09 18:10:47 -0300

    produtos

--- js/produtos.js
+++ js/produtos.js
@@ -8,8 +8,22 @@ $(document).ready(function(){
    CHECKBOX
    ============ */
 function filtroLinha(){
-	$(".labelCheckbox input:checkbox").styleRadioCheckbox({
-		classChecked:"inputCheckboxChecked",
-		classFocus:"inputFocus"
+	$('.labelCheckbox input:checkbox').styleRadioCheckbox({ classChecked: 'inputCheckboxChecked', classFocus: 'inputFocus' });
+	$('.filtroLink input:checkbox').styleRadioCheckbox({ classChecked: 'filtroLinkAtivo', classFocus: 'filtroLinkFocus' });
+
+	var $linhas = $('#filtroLinhas'),
+		$categorias = $('#filtroCategorias'),
+		$animais = $('#filtroAnimais');
+
+	$linhas.on('change.filtro', 'input', function(event) {
+		$linhas.find('input').not($(this)).prop('checked', false).parents('label').removeClass('filtroLinkAtivo');
+		var val = $(this).val();
+		$categorias.show().find('.filtroUl').hide().filter('[data-linha$="'+val+'"]').show();
+	});
+
+	$categorias.on('change.filtro', 'input', function(event) {
+		$linhas.find('input').not($(this)).prop('checked', false).parents('label').removeClass('filtroLinkAtivo');
+		var val = $(this).val();
+		$categorias.show().find('.filtroUl').hide().filter('[data-linha$="'+val+'"]').show();
 	});
 }
